02ccce14664514a4db0d11e1b5b94c14a713c57c
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2016-03-02 16:37:43 -0300

    Cannot concatenate a tuple to a list

--- example/example/settings/base.py
+++ example/example/settings/base.py
@@ -100,9 +100,9 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
+TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + [
     "django.core.context_processors.request",
-)
+]
 
 ROOT_URLCONF = 'example.urls'
 
